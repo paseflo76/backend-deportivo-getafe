165068a0cce207c3acf6223e36ad66ea0f65a647
Pablo Serrano <160110487+paseflo76@users.noreply.github.com>
2025-06-24 18:57:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: 'https://frontend-deportivo-getafe-exlw.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
->>>>>>> nombre-nueva-rama
+
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   })
