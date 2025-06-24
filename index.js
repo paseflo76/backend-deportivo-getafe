45012ec6867274cae7197ddd456d1fa885bcad05
paseflo76 <paseflo34@gmail.com>
2025-06-24 17:29:11 +0200

    cambios

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ app.use(
   })
 )
 
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://frontend-deportivo-getafe-exlw.vercel.app'
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
