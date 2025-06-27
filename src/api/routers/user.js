4018448f1cb3d150fa1cf6813af4164e5ba90f97
Pablo Serrano <160110487+paseflo76@users.noreply.github.com>
2025-06-27 12:19:05 +0200

    Update user.js

--- src/api/routers/user.js
+++ src/api/routers/user.js
@@ -1,24 +1,41 @@
+// backend/routes/userRoutes.js
 const { isAdmin, isAuth } = require('../../middleware/auth')
+const multer = require('multer')
+const path = require('path')
 const {
   getUsers,
+  getUserById,
   register,
   login,
   updateUser,
-  deleteUser
+  deleteUser,
+  uploadAvatar
 } = require('../controller/user')
 
 const userRoutes = require('express').Router()
 
-//? Rutas a los Usuarios
-userRoutes.get('/admin-only', isAuth, isAdmin, (req, res) => {
-  res.status(200).json({ message: 'Bienvenido adminitrador' })
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/avatars')
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname)
+    cb(null, `${req.params.id}-${Date.now()}${ext}`)
+  }
 })
 
-userRoutes.get('/', [isAdmin], getUsers)
+const upload = multer({ storage })
+
+userRoutes.get('/admin-only', isAuth, isAdmin, (req, res) => {
+  res.status(200).json({ message: 'Bienvenido administrador' })
+})
+
+userRoutes.get('/', isAdmin, getUsers)
+userRoutes.get('/:id', isAuth, getUserById)
 userRoutes.post('/register', register)
 userRoutes.post('/login', login)
-userRoutes.put('/:id', [isAdmin], updateUser)
-userRoutes.delete('/:id', [isAuth], deleteUser)
-
+userRoutes.put('/:id', isAuth, updateUser)
+userRoutes.delete('/:id', isAuth, deleteUser)
+userRoutes.post('/:id/avatar', isAuth, upload.single('avatar'), uploadAvatar)
 
 module.exports = userRoutes
