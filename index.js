b0c4ca58af8872d16d31f908e230143f42947fcb
Pablo Serrano <160110487+paseflo76@users.noreply.github.com>
2025-06-27 12:14:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,41 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const eventsRouters = require('./src/api/routers/event')
-const userRoutes = require('./src/api/routers/user')
-const { connecDB } = require('./src/config/db')
+const { isAdmin, isAuth } = require('../../middleware/auth')
+const multer = require('multer')
+const path = require('path')
+const {
+  getUsers,
+  getUserById,
+  register,
+  login,
+  updateUser,
+  deleteUser,
+  uploadAvatar
+} = require('../controller/user')
 
-const cloudinary = require('cloudinary').v2
+const userRoutes = require('express').Router()
 
-const app = express()
-
-/* app.use(cors()) */
-
-app.use(
-  cors({
-    origin: 'https://frontend-deportivo-getafe-exlw.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  })
-)
-
-
-app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://frontend-deportivo-getafe-exlw.vercel.app'
-  )
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  )
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-  )
-  next()
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/avatars')
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname)
+    cb(null, `${req.params.id}-${Date.now()}${ext}`)
+  }
 })
 
+const upload = multer({ storage })
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-connecDB()
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+userRoutes.get('/admin-only', isAuth, isAdmin, (req, res) => {
+  res.status(200).json({ message: 'Bienvenido administrador' })
 })
 
-app.use('/api/v2/eventos', eventsRouters)
-app.use('/api/v2/users', userRoutes)
+userRoutes.get('/', isAdmin, getUsers)
+userRoutes.get('/:id', isAuth, getUserById)
+userRoutes.post('/register', register)
+userRoutes.post('/login', login)
+userRoutes.post('/:id/avatar', isAuth, upload.single('avatar'), uploadAvatar)
+userRoutes.put('/:id', isAuth, updateUser)
+userRoutes.delete('/:id', isAuth, deleteUser)
 
-app.use((req, res) => {
-  return res.status(400).json({ message: 'Route not found' })
-})
+module.exports = userRoutes
 
-/* app.listen(3000, () => {
-  console.log('Servidor funcionando en http://localhost:3000 🎆🎆😊')
-})
- */
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Servidor funcionando en http://localhost:${PORT}`)
-})
