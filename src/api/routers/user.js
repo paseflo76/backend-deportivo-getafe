baa5c243b0cc9410d12cdd63f55e2970ffc82037
paseflo76 <paseflo34@gmail.com>
2025-07-01 10:59:23 +0200

    poner routers par poder ver perfil admin y a todos

--- src/api/routers/user.js
+++ src/api/routers/user.js
@@ -7,7 +7,8 @@ const {
   login,
   updateUser,
   deleteUser,
-  uploadAvatar
+  uploadAvatar,
+  getMyProfile
 } = require('../controller/user')
 
 const userRoutes = require('express').Router()
@@ -20,12 +21,22 @@ userRoutes.get('/admin-only', isAuth, isAdmin, (req, res) => {
   res.status(200).json({ message: 'Bienvenido administrador' })
 })
 
+// Admin ve todos los usuarios
 userRoutes.get('/', isAuth, isAdmin, getUsers)
+
+// Usuario o admin ve su perfil
+userRoutes.get('/me', isAuth, getMyProfile)
+
+// Usuario o admin puede ver usuario por id (si es admin o es el mismo)
 userRoutes.get('/:id', isAuth, getUserById)
+
 userRoutes.post('/register', register)
 userRoutes.post('/login', login)
+
 userRoutes.put('/:id', isAuth, updateUser)
 userRoutes.delete('/:id', isAuth, deleteUser)
+
+// Sube avatar (solo para el propio usuario)
 userRoutes.post('/:id/avatar', isAuth, upload.single('avatar'), uploadAvatar)
 
 module.exports = userRoutes
