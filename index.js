047c0dfd3fa4fa157fdc66c16673a8cfe7ab5bfe
paseflo76 <paseflo34@gmail.com>
2025-11-15 12:45:49 +0100

    cambios en el servidor

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.use((req, res) => {
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Servidor funcionando en http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`)
 })
