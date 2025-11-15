daf0185c2e6fc5e4f0c2aa6b1716319f87009158
paseflo76 <paseflo34@gmail.com>
2025-11-15 12:13:36 +0100

    corregir roters stats

--- src/api/routers/stats.js
+++ src/api/routers/stats.js
@@ -4,11 +4,12 @@ const {
   addJugador,
   addPortero
 } = require('../controller/statsController')
+const { isAuth, isAdmin } = require('../middleware/auth') // asegúrate de la ruta correcta
 
 const statsRouter = Router()
 
-statsRouter.get('/', getStats)
-statsRouter.post('/jugador', addJugador)
-statsRouter.post('/portero', addPortero)
+statsRouter.get('/', getStats) // cualquier usuario puede ver stats
+statsRouter.post('/jugador', [isAuth, isAdmin], addJugador) // solo admin puede agregar/modificar
+statsRouter.post('/portero', [isAuth, isAdmin], addPortero)
 
 module.exports = statsRouter
