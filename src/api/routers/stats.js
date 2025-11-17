4a33994fa80f9d6b3b34ef3acc6b8c69e04966dd
paseflo76 <paseflo34@gmail.com>
2025-11-17 19:11:52 +0100

    coregir router

--- src/api/routers/stats.js
+++ src/api/routers/stats.js
@@ -13,11 +13,11 @@ const { isAuth, isAdmin } = require('../../middleware/auth')
 const statsRouter = Router()
 
 statsRouter.get('/', getStats) // cualquier usuario puede ver stats
-statsRouter.post('/jugador', [isAuth, isAdmin], addJugador)
-statsRouter.post('/portero', [isAuth, isAdmin], addPortero)
-statsRouter.put('/jugador/:id', [isAuth, isAdmin], updateJugador)
-statsRouter.put('/portero/:id', [isAuth, isAdmin], updatePortero)
-statsRouter.delete('/jugador/:id', [isAuth, isAdmin], deleteJugador)
-statsRouter.delete('/portero/:id', [isAuth, isAdmin], deletePortero)
+statsRouter.post('/jugador', isAuth, isAdmin, addJugador)
+statsRouter.post('/portero', isAuth, isAdmin, addPortero)
+statsRouter.put('/jugador/:id', isAuth, isAdmin, updateJugador)
+statsRouter.put('/portero/:id', isAuth, isAdmin, updatePortero)
+statsRouter.delete('/jugador/:id', isAuth, isAdmin, deleteJugador)
+statsRouter.delete('/portero/:id', isAuth, isAdmin, deletePortero)
 
 module.exports = statsRouter
